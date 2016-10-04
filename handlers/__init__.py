afadf6b8846919853ff05f2f710789705b47c1fc
Henrique Lopes <riquellopes@gmail.com>
2016-10-04 13:03:20 -0300

    criando handler de exemplo

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,5 +1,7 @@
 from monk.handler import MonkRegister
 from handlers.epocacosmeticos_handler import EpocaCosmeticosHandler
+from handlers.bloghenriquelopes_handler import BlogHenriqueLopesHandler
 
 MonkRegister\
-    .add(EpocaCosmeticosHandler, disabled=True)
+    .add(EpocaCosmeticosHandler, disabled=True)\
+    .add(BlogHenriqueLopesHandler)
