965aa6026d6719e83d7200280ffa579e7e29713e
Henrique Lopes <riquellopes@gmail.com>
2016-10-04 13:03:27 -0300

    criando handler de exemplo

---
+++ handlers/bloghenriquelopes_handler.py
@@ -0,0 +1,14 @@
+from monk.handler import MonkHandler
+
+
+class BlogHenriqueLopesHandler(MonkHandler):
+    domain = "blog.henriquelopes.com.br"
+
+    def start(self):
+        self.requests("http://blog.henriquelopes.com.br", callback="responses")
+
+    def responses(self, response):
+        """
+            Processa urls da home.
+        """
+        print(response, "Response")
