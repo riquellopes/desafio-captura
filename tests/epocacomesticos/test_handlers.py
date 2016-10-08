ed7df0730af8c162e9a718c0dd7393f507bd9423
Henrique Lopes <riquellopes@gmail.com>
2016-10-08 18:50:44 -0300

    testando handler

---
+++ tests/epocacomesticos/test_handlers.py
@@ -0,0 +1,20 @@
+from handlers.epocacosmeticos_handler import EpocaCosmeticosHandler
+
+
+def test_method_product_match_should_be_return_true_for_valid_urls():
+    assert EpocaCosmeticosHandler.found_product(
+        "http://www.epocacosmeticos.com.br/effaclar-bb-blur-la-roche-posay-base-facial-corretiva/p")
+    assert EpocaCosmeticosHandler.found_product("http://www.epocacosmeticos.com.br/") is False
+    assert EpocaCosmeticosHandler.found_product("http://sieve.com.br/index.html#contact") is False
+
+
+def test_valid_path():
+    assert EpocaCosmeticosHandler.to_valid_path(".") == "http://{}/".format(EpocaCosmeticosHandler.domain)
+    assert EpocaCosmeticosHandler.to_valid_path(
+        "/login?ReturnUrl=%2faccount%2forders") == "http://{}/login?ReturnUrl=%2faccount%2forders".format(
+            EpocaCosmeticosHandler.domain)
+    assert EpocaCosmeticosHandler.to_valid_path(
+        "http://www.epocacosmeticos.com.br/perfumes") == "http://www.epocacosmeticos.com.br/perfumes"
+    assert EpocaCosmeticosHandler.to_valid_path(
+        "http://www.epocacosmeticos.com.br/effaclar-bb-blur-la-roche-posay-base-facial-corretiva/p") ==\
+        "http://www.epocacosmeticos.com.br/effaclar-bb-blur-la-roche-posay-base-facial-corretiva/p"
